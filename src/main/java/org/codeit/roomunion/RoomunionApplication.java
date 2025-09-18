75bd1fe0f7ddc570dbcc985387f07fc75b4fbee9
coen <bht9011@gmail.com>
2025-09-18 22:51:37 +0900

    initial commit

---
+++ src/main/java/org/codeit/roomunion/RoomunionApplication.java
@@ -0,0 +1,13 @@
+package org.codeit.roomunion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomunionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomunionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/codeit/roomunion/RoomunionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codeit.roomunion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomunionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
