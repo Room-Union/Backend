72f98dba2b91be30d47a65b843e2f37a0be7ad91
coen <bht9011@gmail.com>
2025-09-23 23:42:39 +0900

    refactor: jwt 토큰 예외 처리

--- src/main/java/org/codeit/roomunion/auth/adapter/in/filter/JwtAuthenticationFilter.java
+++ src/main/java/org/codeit/roomunion/auth/adapter/in/filter/JwtAuthenticationFilter.java
@@ -1,13 +1,15 @@
 package org.codeit.roomunion.auth.adapter.in.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.codeit.roomunion.auth.domain.model.CustomUserDetails;
 import org.codeit.roomunion.auth.application.service.JwtService;
+import org.codeit.roomunion.auth.domain.model.CustomUserDetails;
+import org.codeit.roomunion.common.exception.ErrorCode;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,6 +19,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -24,6 +27,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void doFilterInternal(
@@ -33,7 +37,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
 
-        if(isNotBearerToken(authHeader)) {
+        if (isNotBearerToken(authHeader)) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -41,10 +45,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String jwtToken = authHeader.substring(7);
         String email = jwtService.extractUsername(jwtToken);
 
-        if(email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             CustomUserDetails userDetails = (CustomUserDetails) userDetailsService.loadUserByUsername(email);
 
-            validateToken(jwtToken, userDetails);
+            validateToken(response, jwtToken, userDetails);
 
             UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                     userDetails,
@@ -58,16 +62,33 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    private void validateToken(String jwtToken, CustomUserDetails userDetails) {
-        if(jwtService.isTokenInvalid(jwtToken, userDetails)) {
-            throw new JwtException("잘못된 토큰입니다."); //TODO 예외 처리
+    private void validateToken(HttpServletResponse response, String jwtToken, CustomUserDetails userDetails) throws IOException {
+        if (jwtService.isTokenInvalid(jwtToken, userDetails)) {
+            setErrorResponse(response, ErrorCode.INVALID_JWT);
+            throw new JwtException("잘못된 토큰입니다.");
         }
         if (jwtService.isTokenExpired(jwtToken)) {
-            throw new JwtException("만료된 토큰입니다."); //TODO 예외 처리
+            setErrorResponse(response, ErrorCode.EXPIRED_JWT);
+            throw new JwtException("만료된 토큰입니다.");
         }
     }
 
     private boolean isNotBearerToken(String authHeader) {
         return authHeader == null || !authHeader.startsWith("Bearer ");
+    }
+
+    private void setErrorResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+        String errorResponse = objectMapper.writeValueAsString(createErrorResponse(errorCode));
+        response.setStatus(errorCode.getStatusCode());
+        response.setContentType("application/json");
+
+        response.getWriter().write(errorResponse);
+    }
+
+    private Map<String, Object> createErrorResponse(ErrorCode errorCode) {
+        return Map.of(
+                "code", errorCode.getCode(),
+                "message", errorCode.getMessage()
+        );
     }
 }
--- src/main/java/org/codeit/roomunion/common/exception/ErrorCode.java
+++ src/main/java/org/codeit/roomunion/common/exception/ErrorCode.java
@@ -7,15 +7,18 @@ public enum ErrorCode {
     // 400
     INVALID_INPUT_VALUE(40001, "입력값이 잘못되었습니다."),
 
-    // 404
-    USER_NOT_FOUND(40401, "유저를 찾을 수 없습니다."),
-
     // 403,
     LOGIN_FAIL(403001, "로그인에 실패했습니다."),
     INVALID_JWT_TOKEN(403002, "jwt 토큰이 유효하지 않습니다."),
+    INVALID_JWT(40303, "잘못된 JWT 토큰입니다."),
+    EXPIRED_JWT(40304, "만료된 JWT 토큰입니다."),
+
+    // 404
+    USER_NOT_FOUND(40401, "유저를 찾을 수 없습니다."),
 
     // 500,
-    INTERNAL_SERVER_ERROR(50001, "Internal Server Error");
+    INTERNAL_SERVER_ERROR(50001, "Internal Server Error"),
+    ;
 
     private final int code;
     private final String message;
