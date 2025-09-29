72122d2745e9e00eb930da183b950dc8e19df137
coen <bht9011@gmail.com>
2025-09-30 01:43:09 +0900

    refactor: User 도메인의 외부 의존성을 갖는 메서드 제거

--- src/main/java/org/codeit/roomunion/user/domain/model/User.java
+++ src/main/java/org/codeit/roomunion/user/domain/model/User.java
@@ -1,8 +1,6 @@
 package org.codeit.roomunion.user.domain.model;
 
 import lombok.Getter;
-import org.codeit.roomunion.common.adapter.out.persistence.entity.UuidEntity;
-import org.codeit.roomunion.common.config.S3.S3Properties;
 
 @Getter
 public class User {
@@ -22,9 +20,5 @@ public class User {
 
     public static User of(Long id, String email, String password, String nickname, Gender gender) {
         return new User(id, email, password, nickname, gender);
-    }
-
-    public String profileImageKey(S3Properties s3Properties, UuidEntity uuidEntity) {
-        return s3Properties.getPath().getProfile() + "/" + uuidEntity.getUuid();
     }
 }
