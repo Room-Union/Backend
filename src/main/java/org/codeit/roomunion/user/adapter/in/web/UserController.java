770c28ddbf63b40ed22f8b6016aef631167052fe
coen <bht9011@gmail.com>
2025-10-01 22:31:31 +0900

    refactor: 회원가입시 이미지 제거

--- src/main/java/org/codeit/roomunion/user/adapter/in/web/UserController.java
+++ src/main/java/org/codeit/roomunion/user/adapter/in/web/UserController.java
@@ -5,8 +5,10 @@ import org.codeit.roomunion.user.adapter.in.web.response.JoinUserResponse;
 import org.codeit.roomunion.user.application.port.in.UserCommandUseCase;
 import org.codeit.roomunion.user.domain.model.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/users")
@@ -19,8 +21,8 @@ public class UserController {
     }
 
     @PostMapping(value = "/sign-up")
-    public ResponseEntity<JoinUserResponse> createUser(@ModelAttribute JoinUserRequest request, @RequestPart(required = false) MultipartFile profileImage) {
-        User user = userCommandUseCase.join(request.toCommand(), profileImage);
+    public ResponseEntity<JoinUserResponse> createUser(@ModelAttribute JoinUserRequest request) {
+        User user = userCommandUseCase.join(request.toCommand());
         return ResponseEntity.ok(JoinUserResponse.from(user));
     }
 }
