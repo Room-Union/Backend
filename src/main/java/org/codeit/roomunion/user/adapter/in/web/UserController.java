10554acdea9b793e4aefde8fb586683bcd0db2c8
coen <bht9011@gmail.com>
2025-10-03 09:38:01 +0900

    refactor: sign-up 컨트롤러 requestBody로 파라미터 받도록 변경

--- src/main/java/org/codeit/roomunion/user/adapter/in/web/UserController.java
+++ src/main/java/org/codeit/roomunion/user/adapter/in/web/UserController.java
@@ -5,10 +5,7 @@ import org.codeit.roomunion.user.adapter.in.web.response.JoinUserResponse;
 import org.codeit.roomunion.user.application.port.in.UserCommandUseCase;
 import org.codeit.roomunion.user.domain.model.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/users")
@@ -21,7 +18,7 @@ public class UserController {
     }
 
     @PostMapping(value = "/sign-up")
-    public ResponseEntity<JoinUserResponse> createUser(@ModelAttribute JoinUserRequest request) {
+    public ResponseEntity<JoinUserResponse> createUser(@RequestBody JoinUserRequest request) {
         User user = userCommandUseCase.join(request.toCommand());
         return ResponseEntity.ok(JoinUserResponse.from(user));
     }
