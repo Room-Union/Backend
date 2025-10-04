b465ef98eb43dcd66a609e33d014306a575f14b7
coen <bht9011@gmail.com>
2025-10-04 14:43:27 +0900

    feat: 이메일 검증 기능 추가

--- src/main/java/org/codeit/roomunion/common/adapter/out/email/EmailSenderImpl.java
+++ src/main/java/org/codeit/roomunion/common/adapter/out/email/EmailSenderImpl.java
@@ -2,14 +2,27 @@ package org.codeit.roomunion.common.adapter.out.email;
 
 import lombok.extern.slf4j.Slf4j;
 import org.codeit.roomunion.common.application.port.out.EmailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class EmailSenderImpl implements EmailSender {
 
+    private final JavaMailSender mailSender;
+
+    public EmailSenderImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
     @Override
     public void send(String to, String subject, String body) {
-        log.info("Sending email to: [{}], subject: [{}], body: [{}]", to, subject, body);
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(body);
+
+        mailSender.send(simpleMailMessage);
     }
 }
--- src/main/java/org/codeit/roomunion/user/adapter/out/persistence/entity/EmailVerificationEntity.java
+++ src/main/java/org/codeit/roomunion/user/adapter/out/persistence/entity/EmailVerificationEntity.java
@@ -14,7 +14,7 @@ public class EmailVerificationEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
