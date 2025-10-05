aeb8c1faf5b47c69e61a1a4f162fc8864baa86f4
coen <bht9011@gmail.com>
2025-10-06 00:14:01 +0900

    fix: 에러코드 관련 수정

--- src/main/java/org/codeit/roomunion/common/exception/BaseErrorCode.java
+++ src/main/java/org/codeit/roomunion/common/exception/BaseErrorCode.java
@@ -4,13 +4,12 @@ import org.springframework.http.HttpStatus;
 
 public interface BaseErrorCode {
 
-    int getCode();
-
     String getMessage();
 
     HttpStatus getStatus();
 
-    default int getStatusValue() {
-        return getStatus().value();
+    default String getCode() {
+        return ((Enum<?>) this).name();
     }
+
 }
