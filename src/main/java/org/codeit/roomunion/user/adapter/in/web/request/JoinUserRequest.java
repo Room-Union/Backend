de7c727a8dce0b11d8f547f43fb9c7f8c31eae49
coen <bht9011@gmail.com>
2025-10-15 22:26:09 +0900

    refactor: categories 리스트 형식으로 받도록 변경

--- src/main/java/org/codeit/roomunion/user/adapter/in/web/request/JoinUserRequest.java
+++ src/main/java/org/codeit/roomunion/user/adapter/in/web/request/JoinUserRequest.java
@@ -4,19 +4,12 @@ import org.codeit.roomunion.meeting.domain.model.enums.MeetingCategory;
 import org.codeit.roomunion.user.domain.command.UserCreateCommand;
 import org.codeit.roomunion.user.domain.model.Gender;
 
-import java.util.Arrays;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-public record JoinUserRequest(String email, String password, String nickname, Gender gender, String categories) {
+public record JoinUserRequest(String email, String password, String nickname, Gender gender, Set<MeetingCategory> categories) {
 
     public UserCreateCommand toCommand() {
-        return UserCreateCommand.of(email, password, nickname, gender, parseCategories(categories));
+        return UserCreateCommand.of(email, password, nickname, gender, categories);
     }
 
-    private Set<MeetingCategory> parseCategories(String categories) {
-        return Arrays.stream(categories.split(","))
-            .map(MeetingCategory::valueOf)
-            .collect(Collectors.toUnmodifiableSet());
-    }
 }
